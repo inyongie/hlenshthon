a1da961e6a048fd566ce4e6a6479cc0d99164179
InYong Chung <inyong89@gmail.com>
2017-03-13 11:27:11 -0400

    Add files via upload
    updating files

---
+++ Scripts/InfoCursor_backup.cs
@@ -0,0 +1,83 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+public class InfoCursor : MonoBehaviour {
+	
+	// These public fields become settable properties in the Unity editor.
+	public GameObject infoCollection;
+	private GameObject textPrefab;
+
+	private MeshRenderer meshRenderer;
+
+	GestureRecognizer recognizer;
+
+	// Use this for initialization
+	void Start()
+	{
+		// Grab the mesh renderer that's on the same object as this script.
+		meshRenderer = this.gameObject.GetComponentInChildren<MeshRenderer>();
+
+		// Set up a GestureRecognizer to detect Select gestures.
+		recognizer = new GestureRecognizer();
+		recognizer.TappedEvent += (source, tapCount, ray) =>
+		{
+			// Do a raycast into the world that will only hit the Spatial Mapping mesh.
+			var headPosition = Camera.main.transform.position;
+			var gazeDirection = Camera.main.transform.forward;
+
+			RaycastHit hitInfo;
+			if (Physics.Raycast(headPosition, gazeDirection, out hitInfo,
+				30.0f, SpatialMapping.PhysicsRaycastMask))
+			{
+				// Move this object's parent object to
+				// where the raycast hit the Spatial Mapping mesh.
+				infoCollection.transform.position = hitInfo.point;
+
+				// Rotate this object's parent object to face the user.
+				Quaternion toQuat = Camera.main.transform.localRotation;
+				toQuat.x = 0;
+				toQuat.z = 0;
+				infoCollection.transform.rotation = toQuat;
+
+				infoCollection.SetActive(true);
+
+
+
+			}
+		};
+		recognizer.StartCapturingGestures();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		// Do a raycast into the world based on the user's
+		// head position and orientation.
+		var headPosition = Camera.main.transform.position;
+		var gazeDirection = Camera.main.transform.forward;
+
+		RaycastHit hitInfo;
+
+		if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+		{
+			// If the raycast hit a hologram...
+			// Display the cursor mesh.
+			meshRenderer.enabled = true;
+
+			// Move the cursor to the point where the raycast hit.
+			this.transform.position = hitInfo.point;
+
+			// Rotate the cursor to hug the surface of the hologram.
+			this.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+		}
+		else
+		{
+			// If the raycast did not hit a hologram, hide the cursor mesh.
+			meshRenderer.enabled = false;
+		}
+	}
+}
+*/
+
